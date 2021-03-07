427b16da362d22148c5683b72c8945f3eedd9d8e
abhishekark17 <abhishekark17@gmail.com>
2021-03-07 23:04:44 +0530

    bakcground subtraction

--- bSubtraction.cpp
+++ bSubtraction.cpp
@@ -9,42 +9,53 @@
 using namespace cv;
 using namespace std;
 
+//This part was used to get the background images with no vehicles.
+/*
+void getBackground(String path){
+	VideoCapture cap(path);
+	int currentFrame=0;
+	String filepath;
+	while(true) {
+		Mat frame;
+		cap>>frame;
+		filepath="images/a"+to_string(currentFrame);
+		filepath=filepath+".jpg";
+		if (currentFrame % 15 ==0){
+			imwrite(filepath,frame);
+		}
+		currentFrame+=1;
+	}
+}
+*/
+
 int main(int argc, char* argv[])
 {
-    String path="video/main3.mp4";
-    Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(2, 3));
-    //create Background Subtractor objects
-    Ptr<BackgroundSubtractor> pBackSub;
-    pBackSub = createBackgroundSubtractorMOG2();
-    VideoCapture capture(path);
-    if (!capture.isOpened()){
-        //error in opening the video input
-        cerr << "Unable to open video"<< endl;
-        return 0;
+    String videopath="video/trafficvideo.mp4";
+   
+    //getBackground(videopath);
+    String imagepath="images/emptyBackground.jpg";
+    VideoCapture cap(videopath);
+    Ptr<BackgroundSubtractor> bsub;
+    bsub=createBackgroundSubtractorMOG2();
+    Mat frame,fmask;
+    Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(3, 3));
+    while(true){
+    	
+    	cap>>frame;
+    	bsub->apply(frame,fmask);
+    	morphologyEx(fmask, fmask,MORPH_ELLIPSE, kernel);
+
+    	imshow("foreground",fmask);
+    	//imshow("normal",frame);
+    	if(waitKey(1)==27) {
+    		destroyAllWindows();
+    		break;
+    	}
+
     }
-    Mat frame, fgMask;
-    while (true) {
-        capture >> frame;
-        if (frame.empty())
-            break;
-        //update the background model
-        pBackSub->apply(frame, fgMask);
-        //get the frame number and write it on the current frame
-        rectangle(frame, Point(10, 2), Point(100,20),
-                  Scalar(255,255,255), -1);
-        stringstream ss;
-        ss << capture.get(CAP_PROP_POS_FRAMES);
-        string frameNumberString = ss.str();
-        putText(frame, frameNumberString.c_str(), Point(15, 15),
-                FONT_HERSHEY_SIMPLEX, 0.5 , Scalar(0,0,0));
-        //show the current frame and the fg masks
-        morphologyEx(fgMask, fgMask,MORPH_OPEN, kernel);
-        imshow("Frame", frame);
-        imshow("FG Mask", fgMask);
-        //get the input from the keyboard
-        int keyboard = waitKey(30);
-        if (keyboard == 'q' || keyboard == 27)
-            break;
-    }
+    destroyAllWindows();
+
+
+    
     return 0;
 }
