7023aa424f1bf4230620def9b7cd948a87cd6673
Abhinav Singhal <abhinavsinghal8130@gmail.com>
2021-03-23 08:30:38 +0530

    Modified Part-2

--- bSubtraction.cpp
+++ bSubtraction.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -6,6 +7,7 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
+#include <opencv2/video/tracking.hpp>
 
 
 using namespace cv;
@@ -13,7 +15,7 @@ using namespace std;
 
 vector<Point2f> inputPts;	// 4 pts taken from user to select the area of interest	//
 vector<Point2f> Pts_dst;	// destination pts.	//
-Mat imgGray; //Background image displayed which will be used for selecting the four points//
+Mat imgGray; //backGroundground image displayed which will be used for selecting the four points//
 
 //	taking input	//
 void getPointsFromUser(int event, int x, int y, int flags, void* userData) {
@@ -22,8 +24,6 @@ void getPointsFromUser(int event, int x, int y, int flags, void* userData) {
 		if (inputPts.size() < 4) inputPts.push_back(Point2f(x, y));
 	}
 }
-
-
 
 Mat warp(Mat img) {
 	//	resetting both vectors	//
@@ -70,8 +70,6 @@ Mat warp(Mat img) {
 	croppedImg = transformedGray(roi);
 
 	waitKey(0);
-
-
 	return croppedImg;
 }
 
@@ -84,77 +82,85 @@ Mat warpWithoutUserInput(Mat image){
 	image = image(roi);
 	return image;
 }
-//This part was used to get the background images with no vehicles.(should be used to get background image at appropriate time)
-/*
-void getBackground(String path){
-	VideoCapture cap(path);
-	int currentFrame=0;
-	String filepath;
-	while(true) {
-		Mat frame;
-		cap>>frame;
-		filepath="images/a"+to_string(currentFrame);
-		filepath=filepath+".jpg";
-		if (currentFrame % 15 ==0){
-			imwrite(filepath,frame);
-		}
-		currentFrame+=1;
-	}
+
+Mat filterImage (Mat img_bw) {
+    //img_bw = img_bw > 5;
+
+    // Define the structuring elements
+    Mat se1 = getStructuringElement(MORPH_ELLIPSE, Size(4, 4));
+    Mat se2 = getStructuringElement(MORPH_ELLIPSE, Size(4, 4));
+
+    // Perform closing then opening
+    Mat mask;
+    morphologyEx(img_bw, mask, MORPH_CLOSE, se1);
+    morphologyEx(mask, mask, MORPH_OPEN, se2);
+
+    // Filter the output
+    Mat out = mask.clone();
+    out.setTo(Scalar(0), mask == 0);
+
+	return out;
 }
-*/
 
-/*
-//This function finds the contours and give the bounding box around the object
-void getContours(Mat image,Mat imgOriginal){
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierarchy;
+Mat performBackgroundSubtraction (Mat& inputFrame, Mat& backGround) {
+	Mat queueDensityImg;
+	absdiff(inputFrame , backGround , queueDensityImg);
+	threshold( queueDensityImg, queueDensityImg, 25, 255, THRESH_BINARY);
+	return queueDensityImg;
+}
 
-	findContours(image,contours,hierarchy,RETR_EXTERNAL,CHAIN_APPROX_SIMPLE);
+Mat performOpticalFlow (Mat& previous, Mat& next ) {
+	Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(5, 5)); 
+
+	//	Define Optical flow parameters	//
+	Mat flow(previous.size(), CV_32FC2);
 	
-	vector<vector<Point>> conPoly(contours.size());
-	vector<Rect> boundRect(contours.size());
+    calcOpticalFlowFarneback(previous, next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+	
+	// For visualization	//
+    Mat flow_parts[2];
+    split(flow, flow_parts);
+    Mat magnitude, angle, magn_norm;
+    cartToPolar(flow_parts[0], flow_parts[1], magnitude, angle, true);
+    normalize(magnitude, magn_norm, 0.0f, 1.0f, NORM_MINMAX);
+    angle *= ((1.f / 360.f) * (180.f / 255.f));
 
-	for(int i=0;i<contours.size();i++){
-		int area = contourArea(contours[i]);
-		if(area>1000 && area<5000){
-			float peri = arcLength(contours[i],true);
-			approxPolyDP(contours[i],conPoly[i],0.02*peri,true);
-			//drawContours(imgOriginal,conPoly,-1,Scalar(255,0,255),2);
-			boundRect[i]=boundingRect(conPoly[i]);
-			rectangle(imgOriginal,boundRect[i].tl(),boundRect[i].br(),Scalar(255,0,255),2);
+	//	To build hsv image	//
+    Mat _hsv[3], hsv, hsv8, bgr;
+	Mat opticalFlowOutput;
+    _hsv[0] = angle;
+    _hsv[1] = Mat::ones(angle.size(), CV_32F);
+    _hsv[2] = magn_norm;
+    merge(_hsv, 3, hsv);
+    hsv.convertTo(hsv8, CV_8U, 255.0);
+	cvtColor(hsv8, bgr, COLOR_HSV2BGR);
+	cvtColor(bgr, opticalFlowOutput, COLOR_BGR2GRAY);
 
-		}
-	}
-
+	//	To process the optical flow output and remove noise	//
+	
+	opticalFlowOutput = filterImage(opticalFlowOutput);
+	morphologyEx(opticalFlowOutput, opticalFlowOutput,MORPH_ELLIPSE, kernel);
+	threshold( opticalFlowOutput, opticalFlowOutput, 12 ,255,THRESH_BINARY);
+	return opticalFlowOutput;
 }
-*/
 
-/*
-//Object detection and application of contour function
-void edgeDetector(Mat image,Mat imgOriginal){
-	Mat imgBlur,imgCanny,imgDil,imgErode;
-	//preprocessing
-	GaussianBlur(image,imgBlur,Size(3,3),3,0);
-	Canny(imgBlur,imgCanny,25,75);
-	Mat kernel=getStructuringElement(MORPH_RECT,Size(3,3));
-	dilate(imgCanny,imgDil,kernel);
+void performOutput (int whitePixels1, int whitePixels2, int totalPixels, int frameNumber, ofstream& file) {
+	float queueDensity = (float) whitePixels1 / (float) totalPixels;
+	float dynamicDensity = (float) whitePixels2 / (float) totalPixels;
 
-	//contour detection
-	getContours(imgDil,imgOriginal);
+	file << (float) frameNumber / (float) (15.0) << "," << queueDensity << "," << dynamicDensity << endl;
+	cout << frameNumber << "," << dynamicDensity << "," << dynamicDensity << "\n";
 
+	return;
 }
-*/
-
-
 
 int main(int argc, char* argv[])
 {	
+	//	Initialize I/O	//
+	String imagepath="images/emptyBackground.jpg";
 	string videopath="video/trafficvideo.mp4";
-	//	read image properly	//
-	if (argc==1){
-		cout<<"Video file should be given as argument"<<endl;
-		
-	}
+	ofstream file("data.csv");	
+	if (argc==1) cout<<"Video file should be given as argument"<<endl;
 	else{
 		string videoFileName=argv[1];
 		videopath = "video/"+videoFileName;
@@ -166,53 +172,47 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 	
-    //getBackground(videopath);
-    String imagepath="images/emptyBackground.jpg";
-    Mat back;
-	back = imread(imagepath);
-    back = warp(back);
 
-    Ptr<BackgroundSubtractor> bsub;
-    bsub=createBackgroundSubtractorMOG2();
-    Mat frame,fmask,frameGray,frameDensity;
-    //int countOnLine1,countOnLine2;
-    Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(3, 3));
-	ofstream file("data.csv");
-	bsub->apply(back,frameDensity);
+    //	backGround :: Fixed background for background subtraction for vehicular density	//
+    Mat backGround = imread(imagepath);
+    backGround = warp(backGround);
+	int totalPixels = backGround.rows * backGround.cols;
+
+	// previous :: First frame of the video to begin optical flow. Later on will be used as previous frame in while loop for optical flow	//
+	Mat previous; cap >> previous;
+	cvtColor(previous , previous, COLOR_BGR2GRAY);
+	previous = warpWithoutUserInput(previous);
+
+
 	file<<"Frame,QueueDensity,DynamicDensity\n";
 	cout<<"Frame,QueueDensity,DynamicDensity\n";
 	int frameNumber=0;
-    while(true){
-    	
-    	cap>>frame;
 
-		frameNumber+=1;
-		//warping individual frames one by one
-		frame=warpWithoutUserInput(frame);
-		cvtColor(frame,frameGray,COLOR_RGB2GRAY,0);
+    while(true){	
+		Mat inputNextFrame; 
+		cap >> inputNextFrame;cap >> inputNextFrame;cap >> inputNextFrame;cap >> inputNextFrame;cap >> inputNextFrame;
+		if (inputNextFrame.empty()) break;
+		frameNumber += 5;	//	taking every 5th frame - Ma'am said we can take every 3rd or 5th	//
 
-    	bsub->apply(frameGray,frameDensity);
-    	morphologyEx(frameDensity, frameDensity,MORPH_ELLIPSE, kernel);
-
-    	absdiff(frameGray,back,fmask);
-    	//imshow("normal",frame);
-    	threshold( fmask, fmask, 30,255,THRESH_BINARY);
-    	//edgeDetector(fmask,fmask);
-		int totalPixels = fmask.rows*fmask.cols;
-		int whitePixels = countNonZero(fmask);
-		int whitePixels2= countNonZero(frameDensity);
-		float density = (float)whitePixels/(float)totalPixels;
-		float dynamicDensity = (float)whitePixels2/(float)totalPixels;
-		file<<(float)frameNumber/(float)(15.0)<<","<<density<<","<<dynamicDensity<<"\n";
-		cout<<frameNumber<<","<<density<<","<<dynamicDensity<<"\n";
+		cvtColor(inputNextFrame, inputNextFrame, COLOR_BGR2GRAY);
+		inputNextFrame = warpWithoutUserInput(inputNextFrame);
+		imshow("Original Video" , inputNextFrame);
 		
-    	// line(fmask, Point(0, 10), Point(fmask.cols, 10), Scalar(255,255,255), 3);
-    	// line(fmask, Point(0, fmask.rows-10), Point(fmask.cols, fmask.rows-10), Scalar(255,255,255), 3);
-		imshow("final",frame);
-		if(waitKey(20)==27) {
-    		break;
-    	}
 
+		Mat queueDensityImg = performBackgroundSubtraction(inputNextFrame, backGround);
+		Mat dynamicDensityImg = performOpticalFlow(previous, inputNextFrame);
+		imshow("Background Subtraction Output" , queueDensityImg);
+		imshow("Optical Flow Output" , dynamicDensityImg);
+
+		int whitePixels1 = countNonZero(queueDensityImg);
+		int whitePixels2= countNonZero(dynamicDensityImg);
+
+		performOutput(whitePixels1, whitePixels2, totalPixels, frameNumber, file);
+		
+		int keyboard = waitKey(1);
+        if (keyboard == 'q' || keyboard == 27) break;
+		previous = inputNextFrame;
+    	
     }
 	file.close();
     destroyAllWindows();
